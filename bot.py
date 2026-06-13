b0306bc899204f44367cd258b0d83608ab72cd1a
Mihika-R-Sarji <mihikarsarji@gmail.com>
2026-06-13 14:10:32 +0530

    Fix weather and quote fetching functions

--- bot.py
+++ bot.py
@@ -2,52 +2,55 @@ import requests
 from datetime import date
 
 def get_weather(city="Thiruvananthapuram"):
-    url=f"https://wttr.in/city?{format}=3"
+    url = f"https://wttr.in/{city}?format=3"
     try:
-        response=request.get(url,timeout=10)
+        response = requests.get(url, timeout=10)
         response.raise_for_status()
         return response.text.strip()
     except Exception as e:
-        return f"weather unavailable({e})"
+        return f"Weather unavailable ({e})"
 
 def get_quote():
-    url=f"hhtps://zenquotes.io/api/random"
+    url = "https://zenquotes.io/api/random"
     try:
-        response=request.get(url,timeout=10)
+        response = requests.get(url, timeout=10)
         response.raise_for_status()
-        data=response.json()
-        quote=data[0]["q"]
-        author=data[0]["a"]
-        return f'"{quote}"-{author}'
+        data = response.json()
+        quote = data[0]["q"]
+        author = data[0]["a"]
+        return f'"{quote}" - {author}'
     except Exception as e:
-        return f"quote unavailable ({e})"
+        return f"Quote unavailable ({e})"
 
 def build_summary():
-    today=date.today().strftime("%A, %d %B %Y")
-    weather=get_weather()
-    quote=get_quote()
-    summary=f"""
-    ========================================================
-      PULSE-Daily summary
-      {today}
-    ========================================================
-      WEATHER
-      {weather}
+    today = date.today().strftime("%A, %d %B %Y")
+    weather = get_weather()
+    quote = get_quote()
 
-      QUOTE
-      {quote}
+    summary = f"""
+========================================================
+PULSE - Daily Summary
+{today}
+========================================================
 
-    ========================================================
-    """
+WEATHER
+{weather}
+
+QUOTE
+{quote}
+
+========================================================
+"""
     return summary
 
-    def run():
-        summary=build_summary()
-        print(summary)
-        with open("daily_summary.txt","w",encoding="utf-8") as f:
-            f.write(summary)
-        
-        print("Pulse ran successfully")
+def run():
+    summary = build_summary()
+    print(summary)
 
-        if _name_=="_main_":
-            run()
+    with open("daily_summary.txt", "w", encoding="utf-8") as f:
+        f.write(summary)
+
+    print("Pulse ran successfully")
+
+if __name__ == "__main__":
+    run()
